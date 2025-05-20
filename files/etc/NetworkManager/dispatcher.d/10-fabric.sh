8430a4b7f3013a1ece6768a6e41772f009f1b214
Philipp Schmitt <philipp.schmitt@wiit.cloud>
2025-05-20 15:45:56 +0200

    Shellcheck

--- files/etc/NetworkManager/dispatcher.d/10-fabric.sh
+++ files/etc/NetworkManager/dispatcher.d/10-fabric.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # NetworkManager dispatcher script to handle dhcp4-change events
 # for interfaces eth2 or eth3, extract wiit_vendor_fabric_ip, and update a connection called "fabric"
@@ -36,24 +36,24 @@ fi
 echo "Found active connection: $CONNECTION_ID for interface $INTERFACE"
 
 if [ -z "$DHCP4_WIIT_VENDOR_FABRIC_IP" ]; then
-    echo "wiit_vendor_fabric_ip not found in DHCP options"
+    echo "wiit_vendor_fabric_ip not found in DHCP options" >&2
     exit 1
 fi
 
 # Get the current connection settings for prefix length and gateway
 if [ -z "$DHCP4_WIIT_VENDOR_FABRIC_CIDR" ]; then
-    echo "Fabric CIDR not set on switch, cannot update"
+    echo "Fabric CIDR not set on switch, cannot update" >&2
     exit 1
 fi
 
 # Check if the "fabric" connection exists
 if ! nmcli connection show "$FABRIC_CONNECTION" &>/dev/null; then
-    echo "Connection '$FABRIC_CONNECTION' does not exist, cannot update"
+    echo "Connection '$FABRIC_CONNECTION' does not exist, cannot update" >&2
     exit 1
 fi
 
 # Check if the fabric ip is already set
-if nmcli -t -f ipv4.addresses connection show "$FABRIC_CONNECTION" | grep -q "$DHCP4_WIIT_VENDOR_FABRIC_IP"; then
+if nmcli -t -f ipv4.addresses connection show "$FABRIC_CONNECTION" | grep -q -- "$DHCP4_WIIT_VENDOR_FABRIC_IP"; then
     echo "Fabric IP is already set, exiting"
     exit 0
 fi
@@ -62,24 +62,23 @@ fi
 echo "Updating $FABRIC_CONNECTION connection with IP: $DHCP4_WIIT_VENDOR_FABRIC_IP"
 
 # Update the connection IP address
-nmcli connection modify "$FABRIC_CONNECTION" ipv4.addresses "$DHCP4_WIIT_VENDOR_FABRIC_IP/$DHCP4_WIIT_VENDOR_FABRIC_CIDR"
-
-# Check if the connection update was successful
-if [ $? -eq 0 ]; then
+if ! nmcli connection modify "$FABRIC_CONNECTION" ipv4.addresses "$DHCP4_WIIT_VENDOR_FABRIC_IP/$DHCP4_WIIT_VENDOR_FABRIC_CIDR"; then
+    echo "Failed to update '$FABRIC_CONNECTION' connection" >&2
+else
     echo "Successfully updated '$FABRIC_CONNECTION' connection"
 
     # Check if the connection is currently active
-    if nmcli -t -f NAME,STATE connection show --active | grep -q "^$FABRIC_CONNECTION:activated"; then
+    if nmcli -t -f NAME,STATE connection show --active | grep -q -- "^$FABRIC_CONNECTION:activated"; then
         echo "Reapplying connection to activate new settings"
         nmcli connection up "$FABRIC_CONNECTION"
     fi
-else
-    echo "Failed to update '$FABRIC_CONNECTION' connection"
 fi
 
-# Write fabric ip into env file
-sed -i -e "s|^FABRIC_IP=.*|FABRIC_IP=${DHCP4_WIIT_VENDOR_FABRIC_IP}|g" /etc/wiit-env.vars
+# NOTE Beware of race-conditions here!
+# Write fabric IP into env file
+sed -i -r -e "s|^FABRIC_IP=.*|FABRIC_IP=${DHCP4_WIIT_VENDOR_FABRIC_IP}|g" /etc/wiit-env.vars
 
-for con in $(nmcli -g NAME,TYPE connection show --active | grep ":802-3-ethernet"); do
-  nmcli -g IP4.GATEWAY device show $(echo $con | cut -d ":" -f 1)
+for CONNECTION in $(nmcli -g NAME,TYPE connection show --active | grep ":802-3-ethernet" | cut -d ":" -f 1); do
+  DEVICE=$(nmcli -g connection.interface-name con show "$CONNECTION")
+  nmcli -g IP4.GATEWAY device show "$DEVICE"
 done | paste -sd "," - | sed -i -e "s|^GATEWAYS=.*|GATEWAYS=${-}|g" /etc/wiit-env.vars
