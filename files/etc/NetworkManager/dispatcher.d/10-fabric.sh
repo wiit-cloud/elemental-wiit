fb1fcdaf4111fa5750d0c6ed06d5ac23af38ac92
Flo <max06.net@outlook.com>
2025-05-20 13:19:15 +0200

    Fix dhcp fabric handling

--- files/etc/NetworkManager/dispatcher.d/10-fabric.sh
+++ files/etc/NetworkManager/dispatcher.d/10-fabric.sh
@@ -12,77 +12,67 @@ ACTION="$2"
 FABRIC_CONNECTION="fabric"
 
 # Log to syslog
-logger -t "fabric-ip-handler" "Called with interface=$INTERFACE, action=$ACTION"
+echo "Called with interface=$INTERFACE, action=$ACTION"
 
 # Exit if the action was triggered by the fabric interface
 if [ "$INTERFACE" == "$FABRIC_CONNECTION" ]; then
-    logger -t "fabric-ip-handler" "Triggered by fabric interface, exiting"
+    echo "Triggered by fabric interface, exiting"
     exit 1
 fi
 
-# Check if this is a dhcp4-change event
-if [ "$ACTION" != "dhcp4-change" ]; then
-    logger -t "fabric-ip-handler" "Not a dhcp4-change event, exiting"
+# Check if this is an "up" event
+if [ "$ACTION" != "up" ]; then
+    echo "Not an up event, exiting"
     exit 0
 fi
 
-# Get active connection name for this interface
-CONNECTION_NAME=$(nmcli -t -f NAME,DEVICE connection show --active | grep ":$INTERFACE" | cut -d: -f1)
-
-if [ -z "$CONNECTION_NAME" ]; then
-    logger -t "fabric-ip-handler" "Could not find active connection for interface $INTERFACE"
+if [ -z "$$CONNECTION_ID" ]; then
+    echo "Could not find active connection for interface $INTERFACE"
     exit 1
 fi
 
-logger -t "fabric-ip-handler" "Found active connection: $CONNECTION_NAME for interface $INTERFACE"
+echo "Found active connection: $$CONNECTION_ID for interface $INTERFACE"
 
-# Get the wiit_vendor_fabric_ip value using nmcli
-FABRIC_IP=$(nmcli -t -f DHCP4.OPTION connection show "$CONNECTION_NAME" | grep "wiit_vendor_fabric_ip" | cut -d= -f2 | tr -d ' ')
+if [ -z "$DHCP4_WIIT_VENDOR_FABRIC_IP" ]; then
+    echo "wiit_vendor_fabric_ip not found in DHCP options"
+    exit 1
+fi
 
-if [ -z "$FABRIC_IP" ]; then
-    logger -t "fabric-ip-handler" "wiit_vendor_fabric_ip not found in DHCP options"
+# Get the current connection settings for prefix length and gateway
+if [ -z "$DHCP4_WIIT_VENDOR_FABRIC_CIDR" ]; then
+    echo "Fabric CIDR not set on switch, cannot update"
     exit 1
 fi
 
 # Check if the "fabric" connection exists
 if ! nmcli connection show "$FABRIC_CONNECTION" &>/dev/null; then
-    logger -t "fabric-ip-handler" "Connection '$FABRIC_CONNECTION' does not exist, cannot update"
+    echo "Connection '$FABRIC_CONNECTION' does not exist, cannot update"
     exit 1
 fi
 
 # Check if the fabric ip is already set
-if nmcli -t -f ipv4.addresses connection show "$FABRIC_CONNECTION" | grep $FABRIC_IP; then
-    logger -t "fabric-ip-handler" "Fabric IP is already set, exiting"
+if nmcli -t -f ipv4.addresses connection show "$FABRIC_CONNECTION" | grep $DHCP4_WIIT_VENDOR_FABRIC_IP; then
+    echo "Fabric IP is already set, exiting"
     exit 0
 fi
 
 # Update the fabric connection with the new IP address
-logger -t "fabric-ip-handler" "Updating '$FABRIC_CONNECTION' connection with IP: $FABRIC_IP"
-
-# Get the current connection settings for prefix length and gateway
-CURRENT_PREFIX=$(nmcli -t -f ipv4.addresses connection show "$FABRIC_CONNECTION" | cut -d: -f2 | grep -o '/[0-9]*' | tr -d '/')
-if [ -z "$CURRENT_PREFIX" ]; then
-    # Default to /32 if no prefix is found
-    CURRENT_PREFIX="32"
-    logger -t "fabric-ip-handler" "No prefix found, using default: $CURRENT_PREFIX"
-else
-    logger -t "fabric-ip-handler" "Using existing prefix: $CURRENT_PREFIX"
-fi
+echo "Updating '$FABRIC_CONNECTION' connection with IP: $DHCP4_WIIT_VENDOR_FABRIC_IP"
 
 # Update the connection IP address
-nmcli connection modify "$FABRIC_CONNECTION" ipv4.addresses "$FABRIC_IP/$CURRENT_PREFIX"
+nmcli connection modify "$FABRIC_CONNECTION" ipv4.addresses "$DHCP4_WIIT_VENDOR_FABRIC_IP/$DHCP4_WIIT_VENDOR_FABRIC_CIDR"
 
 # Check if the connection update was successful
 if [ $? -eq 0 ]; then
-    logger -t "fabric-ip-handler" "Successfully updated '$FABRIC_CONNECTION' connection"
+    echo "Successfully updated '$FABRIC_CONNECTION' connection"
     
     # Check if the connection is currently active
     if nmcli -t -f NAME,STATE connection show --active | grep -q "^$FABRIC_CONNECTION:activated"; then
-        logger -t "fabric-ip-handler" "Reapplying connection to activate new settings"
+        echo "Reapplying connection to activate new settings"
         nmcli connection up "$FABRIC_CONNECTION"
     fi
 else
-    logger -t "fabric-ip-handler" "Failed to update '$FABRIC_CONNECTION' connection"
+    echo "Failed to update '$FABRIC_CONNECTION' connection"
 fi
 
 exit 0
